3135fc6904e41efc63ef17ff369da87a66d05c56
RamuneSpark-Sugedama01 <120969218+RamuneSpark-Sugedama01@users.noreply.github.com>
2024-05-27 05:43:12 +0900

    update

--- js/div_id.js
+++ js/div_id.js
@@ -22,6 +22,38 @@ div_status.style.zIndex = 1001;
 ////出力
 setImage(div_status,"");
 
+document.write( "<div id= mapBase> </div>" );
+const div_mapBase = document.getElementById( "mapBase" );
+defaultSet(div_mapBase,screen);
+////プロパティ
+putXY(div_mapBase,"50%","50%")
+transform(div_mapBase, Center,Center,0,100)
+div_mapBase.style.zIndex = 1001;
+div_mapBase.style.overflow = "hidden";
+////出力
+setImage(div_mapBase,"");
+
+let mapLo = {x:50,y:50};
+document.write( "<div id= map> </div>" );
+const div_map = document.getElementById( "map" );
+defaultSet(div_map,screen);
+////プロパティ
+transform(div_map, Center,Center,0,100)
+div_map.style.zIndex = 1002;
+////出力
+setImage(div_map,"");
+
+let markLo = {x:50,y:50};
+document.write( "<div id= mark> </div>" );
+const div_mark = document.getElementById( "mark" );
+defaultSet(div_mark,div_map);
+////プロパティ
+putXY(div_mark,50+"%",50+"%")
+transform(div_mark, Center,Center,0,100)
+div_mark.style.zIndex = 2000;
+////出力
+setImage(div_mark,"");
+
 //コピペ・テスト用 文字
 document.write( "<div id= text> </div>" );
 const div_text = document.getElementById( "text" );
--- js/interval.js
+++ js/interval.js
@@ -1,6 +1,13 @@
 //ここに書いてある項目が1/50秒に1回実行されます。
 function run(){
     
+if(bs == -100){
+
+    div_mapBase.appendChild(div_map);
+    div_mapBase.appendChild(div_mark);
+    
+}
+
     if(aShift == 0 && bs == 0){
     statusLo.t += 0.04;
 
@@ -26,10 +33,23 @@ function run(){
     
     setImage(div_status,"image/statusBar.png");
  
-    setText(div_text,[Math.round(alpha),Math.round(beta),Math.round(gamma)]+"<br>"+[TEST,ip]); 
+    setText(div_text,[Math.round(alpha)]+"<br>"+[TEST,ip]); 
    
    
+    setImage(div_mapBase,"image/mapBase.png");
 
+    markLo.y = heightGyro();
+    markLo.x = widthGyro();
+    
+    mapLo.y = 100 - markLo.y;
+    mapLo.x = 100 - markLo.x;
+
+
+
+    putXY(div_map,(330/2)+(300)*(mapLo.x-50)*0.01,(220/2)+(200)*(mapLo.y-50)*0.01)
+    setImage(div_map,"image/map.png");
+
+    setImage(div_mark,"image/mark.png");
 
     
 }
@@ -38,12 +58,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const ip = urlParams.get('id');
 
-const socket = io("http://"+ip+":4622/");            
+const socket = io("https://bs3-arcade-server-assist-page.glitch.me/");            
         
 let TEST = "";
 
-socket.on('ipv4', (ipv4) => {
-    TEST = "接続OK";
+socket.on('ipv4', (id) => {
+    TEST = id;
   });
 
 
@@ -51,14 +71,101 @@ let alpha = 0;
 let beta = 0;
 let gamma = 0;
 
+let ox = 0;
+let oz = 0;
+
+let calibration = {mode:0,alpha:null,ox:null,oz:null};
+
 window.addEventListener("deviceorientation", (event)=>{
 
     alpha = event.alpha;
     beta = event.beta;
     gamma = event.gamma;
   
+    if(calibration.mode === 0){
+
+        calibration.mode = 1;
+        calibration.alpha = alpha;
+
+    }
 
 })
 
+
+
+function heightGyro(){
+
+let y = beta-60;
+y *= -1;
+
+if( y >= 20){
+    y = 20;
+}
+if( y <= -20){
+    y = -20;
+}
+
+    let a = 50 + y*(50/20);
+
+
+    return a;
+
+
+}
+
+function widthGyro(){
+
+if(calibration.mode == 0){
+    return 50;
+}
+
+let a = alpha
+let ca = calibration.alpha
+let x = a - ca;
+
+//0 - 360の境目処理、両者の差が180以上の場合（a=0,ca=359など）はループをつなぐ方が近い
+if(a - ca > 180){
+
+x = a - (ca + 360);
+
+}else if(a - ca < -180){
+
+    x = (a + 360) - ca;
+    
+}
+
+
+x *= -1;
+
+let g = gamma;
+g *= 1;
+
+if(x >= 20){
+    x = 20;
+}
+if(x <= -20){
+    x = -20;
+}
+
+if(g >= 30){
+    g = 30
+}
+if(g <= -30){
+    g = -30
+}
+
+let b = 50 + x*(50/20)+g*(50/30);
+
+if(b <= 0){
+    b = 0;
+}
+if(b >= 100){
+    b = 100;
+}
+
+return b;
+
+
+}
 
 
