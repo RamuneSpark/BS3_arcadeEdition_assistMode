ae3e0466699b81dad53a7cc919320948025f89be
RamuneSpark-Sugedama01 <120969218+RamuneSpark-Sugedama01@users.noreply.github.com>
2024-07-07 00:42:37 +0900

    Update interval.js

--- js/interval.js
+++ js/interval.js
@@ -663,6 +663,7 @@ for(let i = 0; i < 2; i++){
         team = i;
         soundName[0] = "select";
         localStorage.setItem("bs3_assistData_621820623",JSON.stringify([id,team]));
+        socket.emit('team',sendData(team+1))
         }
     });
 }
